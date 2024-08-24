a7c90ce87fd52a19ff49474f2f4d4b75cfe2fd90
HardWorkers2 <accwebdmytro38140@gmail.com>
2024-08-24 12:49:46 +0300

    Add auto-transition for block with info in home-section

--- script/article-info.js
+++
@@ -1,7 +0,0 @@
-const infoContainers = document.querySelectorAll(".right-content__info");
-
-infoContainers.forEach((infoItem) => {
-  infoItem.addEventListener("click", () => {
-    infoItem.classList.toggle("unexpanded");
-  });
-});
---
+++ script/home-info.js
@@ -0,0 +1,37 @@
+const infoContainers = document.querySelectorAll(".right-content__info");
+let timeoutId;
+let userClicked = false;
+let animationIndex = 0;
+
+infoContainers.forEach((infoItem) => {
+  infoItem.addEventListener("click", () => {
+    infoItem.classList.toggle("unexpanded");
+    userClicked = true;
+    clearTimeout(timeoutId);
+
+    setTimeout(() => {
+      userClicked = false;
+      animationIndex = 0;
+      animateSequentially();
+    }, 3000);
+  });
+});
+
+function animateSequentially() {
+  animationIndex >= infoContainers.length ? (animationIndex = 0) : "";
+  const currentElement = infoContainers[animationIndex];
+  currentElement.classList.add("unexpanded");
+
+  timeoutId = setTimeout(() => {
+    currentElement.classList.remove("unexpanded");
+
+    if (!userClicked) {
+      animationIndex++;
+      setTimeout(() => {
+        animateSequentially();
+      }, 500);
+    }
+  }, 3000);
+}
+
+animateSequentially();
